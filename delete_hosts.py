122ba45346386945858f5e5e7be9d1ec16b6f0a5
Venugopal P <76810281+Venuvgp19@users.noreply.github.com>
2021-11-29 14:28:30 -0600

    Update delete_hosts.py

--- delete_hosts.py
+++ delete_hosts.py
@@ -4,15 +4,18 @@ import requests
 import sys
 import getpass
 
+#collect username and password from end user
+
 username = input('Username : ')
 password = getpass.getpass(prompt='Password : ')
 
+
 class Satellite:
     def __init__(self,servlist):
-	self.servlist = servlist
-	self.sat_server = "wppladm006.supervalu.com"
+	self.servlist = servlist #constructor
+	self.sat_server = ""  #Satellite server FQDN goes here (constructor)
 		
-    def delete_host(self):
+    def delete_host(self): #method of class Satellite
 	if svr and len(self.servlist) == 3:
             for server in self.servlist:
 	        Server_ID = os.popen('curl -s -u %s'%username + ':%s'%password + ' -L http://%s'%self.sat_server +'/api/v2/hosts/%s'%server + ' | json_reformat|grep -m20 \"id\"|grep -w id').read()
@@ -34,19 +37,18 @@ class Satellite:
 		    resp = os.popen('curl -X DELETE -s -k -u  migration:%s'%password + ' -L http://%s'%self.sat_server +'/api/v2/hosts/%s'%str_int + ' |json_reformat').read()
 		    return resp
 		else:
-		    #print response
 		    return ID
 
 if __name__ == '__main__':
     svr = sys.argv[1:]
     if svr:
-        FQDN1 = svr[0]+'.supervalu.com'
-	FQDN2 = svr[0]+'.unfi.com'
-	FQDN3 = svr[0]+'.albertsons.com'
+        FQDN1 = svr[0]+'.example1.com'
+	FQDN2 = svr[0]+'.example2.com'
+	FQDN3 = svr[0]+'.example3.com'
 	servlist = [FQDN1,FQDN2,FQDN3]
 	S = Satellite(servlist)
 	print(S.delete_host())
 
     else:
-	print('please enter server name to delete from satellite : example - eplnx421')
+	print('please enter server name to delete from satellite : example - <server_name>')
 	sys.exit(0)
